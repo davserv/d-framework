5e25c1d97b8ea7bf6a93cb4b14876765bede1e38
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2024-07-04 13:53:03 -0300

    1.1
    1.1

---
+++ web/index.js
@@ -0,0 +1,12 @@
+function heScript() {
+	console.log('Hello world!');
+	console.log('Brazil');
+	console.log(30);
+	console.log('15 + 15');
+	var totalsoma = Number('15') + Number('15');
+	console.log(totalsoma);
+	console.log('02 x 15');
+	var totalmulti = Number('2') * Number('15');
+	console.log(totalmulti);
+}
+heScript()
